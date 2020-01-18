a1c593ab77e102a159a49aa039850a02c6739155
Xavier Francisco <xavier.n.francisco@gmail.com>
2020-01-18 15:56:11 +0000

    Use conftest to load hypothesis profile

---
+++ conftest.py
@@ -0,0 +1,17 @@
+"Introduces pytest fixtures that are necessary to run the test suite."
+
+import os
+from datetime import timedelta
+
+from hypothesis import settings
+import pytest  # type: ignore
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env() -> None:
+    "Fixtures to load the hypothesis profile inside for all tests"
+    settings.register_profile("ci", max_examples=1000, deadline=None)
+    settings.register_profile(
+        "dev", max_examples=100, deadline=timedelta(milliseconds=500)
+    )
+    settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "dev"))
--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"Entrypoint for homie-spec tests"
-
-import os
-from datetime import timedelta
-from hypothesis import settings
-
-settings.register_profile("ci", max_examples=1000, deadline=None)
-settings.register_profile("dev", max_examples=100, deadline=timedelta(milliseconds=500))
-settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "dev"))
