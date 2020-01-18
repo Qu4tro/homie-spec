e75478571c64329914c0df5d4725a71a19cb6782
Xavier Francisco <xavier.n.francisco@gmail.com>
2020-01-18 16:14:08 +0000

    Update pyproject and conftest CI max-examples

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ import pytest  # type: ignore
 @pytest.fixture(scope="session", autouse=True)
 def load_env() -> None:
     "Fixtures to load the hypothesis profile inside for all tests"
-    settings.register_profile("ci", max_examples=1000, deadline=None)
+    settings.register_profile("ci", max_examples=300, deadline=None)
     settings.register_profile(
         "dev", max_examples=100, deadline=timedelta(milliseconds=500)
     )
